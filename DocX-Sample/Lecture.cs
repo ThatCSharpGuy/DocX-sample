14dbed588523d48a82eb1228ab66f03286e111be
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-02-12 18:27:19 -0600

    Preparando el terreno
    Clases auxiliares y referencias

---
+++ DocX-Sample/Lecture.cs
@@ -0,0 +1,10 @@
+﻿namespace DocX_Sample
+{
+    public class Lecture
+    {
+        public int Id { get; set; }
+        public int TeacherId { get; set; }
+        public string Name { get; set; }
+        public string Level { get; set; }
+    }
+}
---
+++ DocX-Sample/Teacher.cs
@@ -0,0 +1,11 @@
+﻿namespace DocX_Sample
+{
+    public class Teacher
+    {
+        public string GivenName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+    }
+}
